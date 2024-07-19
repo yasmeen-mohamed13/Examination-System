a5bd7a23bf9b89e6b49f050d792a1bd5f509d428
KH <KH@DESKTOP-7598236>
2024-07-19 17:16:38 +0300

    Add project files.

---
+++ Examination system/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examination_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
