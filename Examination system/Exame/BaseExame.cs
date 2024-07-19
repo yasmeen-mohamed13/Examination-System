3c25ce1459653da1f4438b55defdf5f2236f8367
KH <KH@DESKTOP-7598236>
2024-07-19 17:27:40 +0300

    update BaseExame class

--- Examination system/Exame/BaseExame.cs
+++ Examination system/Exame/BaseExame.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Examination_system.Exame
 {
-    internal class BaseExame
+    abstract class BaseExame
     {
+        public int Time { get; set; }
+        public int NumOfQuestions { get; set; }
+        protected BaseExame()
+        {
+            
+        }
+        protected BaseExame(int Time,int NumOfQuestions)
+        {
+            this.Time = Time;
+            this.NumOfQuestions = NumOfQuestions;
+        }
+        public abstract void Display();
+
+        public void ShowTimeAndNumOfQuestions(int Time, int NumOfQuestions)
+        {
+            Console.WriteLine($"Time: {Time}, Number of Questions: {NumOfQuestions}");
+        }
     }
 }
