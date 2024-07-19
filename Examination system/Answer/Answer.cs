ab0f3fe4d81bbfffee9a8de331215a296a8236ec
KH <KH@DESKTOP-7598236>
2024-07-19 17:23:04 +0300

    update answer class

--- Examination system/Answer/Answer.cs
+++ Examination system/Answer/Answer.cs
@@ -8,5 +8,17 @@ namespace Examination_system.Answer
 {
     internal class Answer
     {
+        public int Id { get; set; }
+        public string? AnswerText { get; set; }
+
+        public Answer()
+        {
+
+        }
+        public Answer(int Id, string AnswerText)
+        {
+            this.Id = Id;
+            this.AnswerText = AnswerText;
+        }
     }
 }
